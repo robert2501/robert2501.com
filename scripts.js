ed163c81b45e3d88a2e43de7affb3fd512fa4d73
robert2501 <98714056+robert2501@users.noreply.github.com>
2022-01-30 23:26:05 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-var button = document.querySelector(".button");
-var image = document.querySelector(".img-lucrare");
-var wasClicked = false;
-
-button.addEventListener("click", function() {
-   if(!wasClicked) {image.style.width = "800px"; wasClicked = true;} else 
-{image.style.width = "451px"; wasClicked = false;} }, false);
